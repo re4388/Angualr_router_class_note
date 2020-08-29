89c4c706929407f2a48efa00c0ab3e6acfc688d7
re4388 <re4388@gmail.com>
2020-08-29 15:42:52 +0800

    formarray

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit, OnDestroy {
-  // we have this data here for default value, and sometimes, we may even get data
-  // from server to user level default value
   data: any = {
     firstName: 'Will',
     lastName: 'Huang',
@@ -39,9 +37,14 @@ export class RegisterComponent implements OnInit, OnDestroy {
       lastName: this.fb.control('Huang', {
         validators: [Validators.required, Validators.email]
       }),
-      email: this.fb.control('doggy.huang@gmail.com', {
-        validators: [Validators.required]
-      }),
+      emails: this.fb.array([
+        this.fb.control('doggy.huang@gmail.com', {
+          validators: [Validators.required]
+        }),
+        this.fb.control('abc@example.com', {
+          validators: [Validators.required]
+        }),
+      ]),
       password: this.fb.control('', {
         validators: [Validators.required, Validators.minLength(6)]
       }),
@@ -49,6 +52,7 @@ export class RegisterComponent implements OnInit, OnDestroy {
         validators: [Validators.required, Validators.minLength(6)]
       })
     });
+
   }
 
   ngOnDestroy(): void {
@@ -74,18 +78,24 @@ export class RegisterComponent implements OnInit, OnDestroy {
     }
   }
 
-  fc(name: string): FormControl {
-    // .get here is AbstractControl, we convert to FormControl
-    return this.form.get(name) as FormControl;
-  }
-  // <ul[class.invalid - feedback]="fc('firstName').touched && fc('firstName').invalid"
-  // fieldChecker(name: string): boolean {
-  //   return this.form.get(name).touched && this.form.get(name).invalid;
-  // }
-
   isInvalid(name: string): boolean {
     return (this.fc(name).touched || this.fc(name).dirty)
       && this.fc(name).invalid;
+  }
+
+  fc(name: string): FormControl {
+    return this.form.get(name) as FormControl;
+  }
+
+  fa(name: string): FormArray {
+    return this.form.get(name) as FormArray;
+  }
+
+  addNewEmail(): void {
+    const emails = this.fa('emails');
+    emails.push(this.fb.control('', {
+      validators: [Validators.required, Validators.email]
+    }));
   }
 
 }
